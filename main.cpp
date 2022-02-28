3f4f21595e678b07288b54bbb6d9f312f8fa4ba3
max-benson <58283777+max-benson@users.noreply.github.com>
2022-02-27 20:31:48 -0800

    Update main.cpp
    Add insert after and remove functions

--- main.cpp
+++ main.cpp
@@ -24,7 +24,12 @@ public:
 };
 
 void Prepend(int item, Node** ptrToHead);
+bool InsertAfter(int itemToInsert, int afterItem, Node* head);
+bool Remove(int item, Node** ptrToHead);
 void Print(Node* head);
+bool RemoveNotInHead(int item, Node* head);
+Node* FindItem(int item, Node* head);
+Node* FindItemBefore(int item, Node* head);
 
 int main() {
     Node *head = nullptr;
@@ -34,8 +39,9 @@ int main() {
         string line;
         stringstream ss;
         int item;
+        int after;
 
-        cout << "Enter P(repend) number, D(isplay), or E(xit)";
+        cout << "Enter P(repend) number, I(nsert) number after, R(emove) number, D(isplay), or E(xit)";
         getline(cin, line);
         ss.str(line);
         ss >> command;
@@ -47,6 +53,29 @@ int main() {
                 }
                 else {
                     Prepend( item, &head );
+                }
+                break;
+            case 'I':
+                ss >> item;
+                ss >> after;
+                if (ss.fail()) {
+                    cerr << "You need to enter two numbers after 'I'" <<endl;
+                }
+                else {
+                    if (!InsertAfter(item, after, head)) {
+                        cerr << after << " does not appear in the list" << endl;
+                    }
+                }
+                break;
+            case 'R':
+                ss >> item;
+                if (ss.fail()) {
+                    cerr << "You need to enter a number after 'R'" <<endl;
+                }
+                else {
+                    if (!Remove( item, &head )) {
+                        cerr << item << " does not appear in the list" << endl;
+                    }
                 }
                 break;
             case 'D':
@@ -71,6 +100,36 @@ void Prepend(int item, Node** ptrToHead) {
     *ptrToHead = newHead;
 }
 
+bool InsertAfter(int itemToInsert, int afterItem, Node* head) {
+    Node* ptr = FindItem(afterItem, head);
+
+    if (ptr != nullptr) {
+        Node* newNode = new Node;
+        newNode->item = itemToInsert;
+        newNode->next = ptr->next;
+        ptr->next = newNode;
+        return true;
+    }
+    return false;
+}
+
+bool Remove(int item, Node** ptrToHead) {
+    Node* head = *ptrToHead;
+
+    if (nullptr == head) {
+        return false;
+    }
+    else if (head->item == item) {
+        Node* nodeToDelete = head;	// Save pointer to node to be deleted
+        *ptrToHead = head->next;	// Update pointer to head
+        delete nodeToDelete;		// Deallocate the old head node
+        return true;
+    }
+    else {
+        return RemoveNotInHead(item, head);
+    }
+}
+
 void Print(Node* head) {
     cout << "[";
     for (Node* ptr = head; ptr != nullptr; ptr = ptr->next) {
@@ -80,4 +139,41 @@ void Print(Node* head) {
         cout << ptr->item;
     }
     cout << "]" << endl;
+}
+
+bool RemoveNotInHead(int item, Node* head) {
+    Node* nodeToDelete;
+    Node* nodeBefore = FindItemBefore(item, head);
+
+    if (nullptr == nodeBefore) {
+        return false;
+    }
+    nodeToDelete = nodeBefore->next;
+    nodeBefore->next = nodeToDelete->next;
+    delete nodeToDelete;
+    return true;
+}
+
+Node* FindItem(int item, Node* head) {
+    // Starting at "head" search the linked list for a node containing item
+    // If found, return a pointer to the node, otherwise return nullptr
+    for (Node* ptr = head; ptr != nullptr; ptr = ptr->next) {
+        if (ptr->item == item) {
+            return ptr;
+        }
+    }
+    return nullptr;
+}
+
+Node* FindItemBefore(int item, Node* head) {
+    // Starting at "head" search the linked list for a node    // containing item.  If found return a pointer to the node before
+    // before, otherwise return nullptr
+    if (head != nullptr) {
+        for (Node* ptr = head; ptr->next != nullptr; ptr = ptr->next) {
+            if (ptr->next->item == item) {
+                return ptr;
+            }
+        }
+    }
+    return nullptr;
 }
